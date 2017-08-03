a88c88b5f5b62f4355e2c621272c2fe457471e77
Robert Bisewski <contact@ibiscybernetics.com>
2017-08-03 15:10:00 -0500

    adding further code to test perihelion calculations

--- goplex.go
+++ goplex.go
@@ -25,8 +25,11 @@ import (
 //
 var (
 
-    // speed of light in a vaccuum, in m/s
+    // Speed of light in a vaccuum, in m/s
     c float64 = 299792458.0
+
+    // Universal gravitational constant, in m^3 kg^-1 s^-2
+    universal_gravitation_constant float64 = 0.0000000000667408
 )
 
 //! Function to calculate the perihelion shift of an orbit
@@ -69,17 +72,19 @@ func main() {
     fmt.Println("Goplex tests begin now...");
 
     //
-    // Perihelion Shift Calculation
+    // Perihelion Shift Calculation, of Mercury
     //
     var perihelion_shift_expected_result float64 = 0
-    var L float64 = 0
-    var T float64 = 0
-    var e float64 = 0
+    var L float64 = 57909050.0
+    var T float64 = 47.362
+    var e float64 = 0.205630
     var perihelion_shift_test float64 = perihelion_shift(L, T, e)
 
     // do a quick check to ensure this actually gave the right result
     if perihelion_shift_test != perihelion_shift_expected_result {
         fmt.Println("Perihelion Shift test failed!")
+        fmt.Println("Expected: ", perihelion_shift_expected_result)
+        fmt.Println("Calculated: ", perihelion_shift_test)
         os.Exit(1)
     }
 
